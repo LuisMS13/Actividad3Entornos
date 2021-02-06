a25cfaf70b69470eafd32dfbf5cdf49025321304
Luis Moya <lmoyaserrano74@gmail.com>
2021-02-06 19:52:29 +0100

    Workspace Videojuego/Mascota Actividad1

--- Actividad_git/Actividad1/src/dam/mascota/Mascota.java
+++ Actividad_git/Actividad1/src/dam/mascota/Mascota.java
@@ -3,24 +3,5 @@ package dam.mascota;
 public class Mascota {
 	private String id;
 	private String nombre;
-	private String compania;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getCompania() {
-		return compania;
-	}
-	public void setCompania(String compania) {
-		this.compania = compania;
-	}
+	private String color;
 }
