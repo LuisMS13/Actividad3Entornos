9a550629b98c66804b409e35fa32159e5e703b70
Luis Moya <lmoyaserrano74@gmail.com>
2021-02-23 11:14:57 +0100

    Git para actividad 3

---
+++ Actividad_git/Actividad2/src/pojo/coche/Coche.java
@@ -0,0 +1,37 @@
+package pojo.coche;
+
+public class Coche {
+	
+	private String id;
+	private String matriucla;
+	private String modelo;
+	private String marca;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getMatriucla() {
+		return matriucla;
+	}
+	public void setMatriucla(String matriucla) {
+		this.matriucla = matriucla;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+
+}
